758bb6c15368ec7976250c72f8ee21bb004bb41d
ckelner <chris.kelner@gmail.com>
2017-08-11 12:29:20 -0400

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+# Sonarqube
+
+This is a quick hack for a Motorola meeting, but I wanted to capture my efforts.
+
+## Prerequisites
+
+- Install docker
+
+## Run
+
+- `bash run.sh <your-datadog-api-key>`
+  - This will run two docker containers, one w/ sonarqube and one with the docker-dd-agent
+- Connect to localhost:9000
+- Download the CLI scanner at https://docs.sonarqube.org/display/SCAN/Analyzing+with+SonarQube+Scanner
+  - Update sonar-scanner.properties to point to `localhost:9000`
+- Choose a project to scan or download the examples at https://github.com/SonarSource/sonar-examples/zipball/master
+  - I suggest the [projects/languages/generic-coverage/sonar-runner](https://github.com/SonarSource/sonar-examples/tree/master/projects/languages/generic-coverage/sonar-runner) project
+- Change into the directory where the code and configuration is
+- Run `<path-to-executable>/sonar-scanner`
+- You can find JMX metrics under the `jmx.sonarqube...` namespace!
---
+++ run.sh
@@ -0,0 +1,35 @@
+# !/bin/bash
+
+if [ -z $1 ]; then
+  echo "No API key passed, exiting"
+  exit 1
+fi
+
+PWD=$(pwd)
+# NOTE: Kelnersux => You'll need -Djava.rmi.server.hostname=<ip-from-docker>
+# to connect via jconsole... You can find it in the jmx.yaml file after you
+# run this script. Generally 172.17.0.x on OSX w/ the latest version as of Aug 10 2017
+J_OPTS="-Dcom.sun.management.jmxremote= -Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.rmi.port=9998 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.local.only=false"
+
+docker stop sonarqube-sandbox
+docker rm sonarqube-sandbox
+
+docker run -d --name sonarqube-sandbox \
+  -p 9000:9000 -p 9999:9999 -p 9998:9998 \
+  -e SONARQUBE_WEB_JVM_OPTS="${J_OPTS}" \
+  -e JAVA_OPTIONS="${J_OPTS}" \
+  sonarqube:latest
+
+ADDR=$(docker exec sonarqube-sandbox hostname -i)
+sed -i -e "s/localhost/${ADDR}/" ./dd-agent-conf.d/jmx.yaml
+
+docker stop dd-agent-sonar
+docker rm dd-agent-sonar
+
+docker run -d --name dd-agent-sonar \
+   -v /var/run/docker.sock:/var/run/docker.sock:ro \
+   -v /proc/:/host/proc/:ro \
+   -v /sys/fs/cgroup/:/host/sys/fs/cgroup:ro \
+   -v $PWD/dd-agent-conf.d:/conf.d:ro \
+   -e API_KEY=$1 \
+   datadog/docker-dd-agent:latest-jmx
